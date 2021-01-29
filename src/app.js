b1d69c9f4de76dae08936050e4b02b330684032f
Wallis Kassiwin <kassiwin@gmail.com>
2021-01-29 17:35:50 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./style/tailwind.css";
+import "./style/fonts.css";
+
+function App() {
+    return (
+        <div className="min-h-screen min-w-screen grid place-items-center">
+            <div className="text-6xl text-center tracking-wide font-nunito"> Quick start React with tailwind CSS</div>
+        </div>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+        fontFamily: {
+            'nunito': ['nunito', 'sans-serif'],
+            'lato': ['Lato', 'sans-serif'],
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
